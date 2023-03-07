8bff5dfe5582965273ab9ac7e06e4c7bdbdb0603
alexndra1272 <alexandrag1272@gmail.com>
2023-03-07 11:04:36 -0600

    Insertar vuelos

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from sqlalchemy import create_engine
+from flask import Flask, render_template, request, redirect
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 from dotenv import load_dotenv
 import os
@@ -19,3 +19,19 @@ db = scoped_session(sessionmaker(bind=engine))
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/add", methods=["POST", "GET"])
+def add():
+    if request.method == "POST":
+        origen = request.form.get("origin")
+        destino = request.form.get("destination")
+        duracion = request.form.get("duration")
+        if not origen or not destino or not duracion:
+            return "Rellene todos los campos"
+        sql = text("INSERT INTO flights (origin, destination, duration) VALUES (:origin, :destination, :duration)")
+        db.execute(sql,
+                {"origin": origen, "destination": destino, "duration": duracion})
+        db.commit()
+        return redirect("/")
+    return render_template("add.html")
